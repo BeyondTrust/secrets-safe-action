350b9fb582568d6ba7e92a8e108c0b54a52ca0a9
btfhernandez <133419363+btfhernandez@users.noreply.github.com>
2023-10-13 16:29:31 -0500

    feat: improve main.py file (#2)

--- README.md
+++ README.md
@@ -1,17 +1,59 @@
-## Set up project using Docker compose
+# secrets safe action
 
-- Install Docker: [https://docs.docker.com/engine/install/](https://docs.docker.com/engine/install/)
-- Install Docker Compose: [https://docs.docker.com/compose/install/](https://docs.docker.com/compose/install/)
-- Clone repository
-  ```bash
-  git clone https://github.com/BeyondTrust/ps-integration-library.git
-  ```
-- Edit .env file, including needed data.
-- Build the image using docker compose
-  ```bash
-  docker-compose build
-  ```
-- Run the image using docker compose
-  ```bash
-  docker-compose up
-  ```
+This action retrieves secrets from BeyondTrtust Pasword Safe API
+
+## Inputs
+
+### `client_id`
+
+**Required** API OAuth Client ID.
+
+### `client_secret`
+
+**Required** API OAuth Client Secret.
+
+### `api_url`
+
+**Required** BeyondTrust Password Safe API URL.
+
+### `verify_ca`
+
+Indicates whether to verify the certificate authority on the Secrets Safe instance.
+
+### `secret_path`
+
+**Required** Secret to retrieve.
+
+### `managed_account_path`
+
+**Required** Managed account to retrieve.
+
+### `certificate`
+
+Password Safe API Certificate Content. For use when authenticating using a Client Certificate.
+
+### `certificate_key`
+
+Password Safe API Certificate Key. For use when authenticating using a Client Certificate.
+
+## Outputs
+
+### `secret`
+
+Retrieved secret.
+
+## Example usage
+
+```yaml
+uses: BeyondTrust/secrets-safe-action@v1
+env:
+  API_URL: ${{vars.API_URL}}
+  VERIFY_CA: ${{vars.VERIFY_CA}}
+  CLIENT_ID: ${{secrets.CLIENT_ID}}
+  CLIENT_SECRET: ${{secrets.CLIENT_SECRET}}
+  CERTIFICATE: ${{secrets.CERTIFICATE}}
+  CERTIFICATE_KEY: ${{secrets.CERTIFICATE_KEY}}
+with:
+  SECRET_PATH: 'secret_folder/secret_title'
+  MANAGED_ACCOUNT_PATH: 'system_name/managed_account_name'
+```
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 import logging
-from secrets_safe_library import secrets_safe, authentication, utils
+from secrets_safe_library import secrets_safe, authentication, utils, managed_account
+import uuid
 
 env = os.environ
 
-PASSWORD_SAFE_CLIENT_ID = env["CLIENT-ID"] if 'CLIENT-ID' in env else None
-PASSWORD_SAFE_CLIENT_SECRET = env["CLIENT-SECRET"] if 'CLIENT-SECRET' in env else None
-PASSWORD_SAFE_API_URL = env["API_URL"] if 'API_URL' in env else None
-VERIFY_CA = True if 'VERIFY-CA' in env and env['VERIFY-CA'].lower() == 'true' else False
-SECRET = env['INPUT_SECRET'].strip() if 'INPUT_SECRET' in env else None
-SECRET_LIST = env['SECRET_LIST'].strip().split(",") if 'SECRET_LIST' in env and env['SECRET_LIST'].strip() != "" else None
+CLIENT_ID = env["CLIENT_ID"] if 'CLIENT_ID' in env else None
+CLIENT_SECRET = env["CLIENT_SECRET"] if 'CLIENT_SECRET' in env else None
+API_URL = env["API_URL"] if 'API_URL' in env else None
+VERIFY_CA = True if 'VERIFY_CA' in env and env['VERIFY_CA'].lower() == 'true' else False
 
-PATH_SEPARATOR = env['PATH_SEPARATOR'].strip() if 'PATH_SEPARATOR' in env else "/"
+SECRET_PATH = env['INPUT_SECRET_PATH'].strip() if 'INPUT_SECRET_PATH' in env else None
+MANAGED_ACCOUNT_PATH = env['INPUT_MANAGED_ACCOUNT_PATH'].strip() if 'INPUT_MANAGED_ACCOUNT_PATH' in env else None
+
+PATH_SEPARATOR = env['PATH_SEPARATOR'].strip() if 'PATH_SEPARATOR' in env and len(env['PATH_SEPARATOR'].strip()) == 1 else "/"
 
 LOGGER_NAME = "custom_logger"
 
@@ -25,11 +27,20 @@ logger = logging.getLogger(LOGGER_NAME)
 CERTIFICATE = env['CERTIFICATE'].replace(r'\n', '\n') if 'CERTIFICATE' in env else None
 CERTIFICATE_KEY = env['CERTIFICATE_KEY'].replace(r'\n', '\n') if 'CERTIFICATE_KEY' in env else None
 
+
+def append_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        delimiter = uuid.uuid1()
+        print(f'{name}<<{delimiter}', file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
+        
+
 def main():
     try:
-        authentication_obj = authentication.Authentication(PASSWORD_SAFE_API_URL, 
-                                                    PASSWORD_SAFE_CLIENT_ID, 
-                                                    PASSWORD_SAFE_CLIENT_SECRET,
+        authentication_obj = authentication.Authentication(API_URL, 
+                                                    CLIENT_ID, 
+                                                    CLIENT_SECRET,
                                                     CERTIFICATE,
                                                     CERTIFICATE_KEY,
                                                     VERIFY_CA,
@@ -38,19 +49,24 @@ def main():
         
         get_api_access_response = authentication_obj.get_api_access()
         
-        if get_api_access_response.status_code == 200:
-                secrets_safe_obj = secrets_safe.SecretsSafe(authentication=authentication_obj, logger=logger, separator=PATH_SEPARATOR)
-                if SECRET or SECRET_LIST:
-                    if SECRET:
-                        get_secret_response = secrets_safe_obj.get_secret(SECRET)
-                        utils.print_log(logger, f"=> Retrive secret: {get_secret_response}", logging.DEBUG)
-                    if SECRET_LIST:
-                        get_secrets_response = secrets_safe_obj.get_secrets(SECRET_LIST)
-                        utils.print_log(logger, f"=> Retrive secrets: {get_secrets_response}", logging.DEBUG)
-                else:
-                    utils.print_log(logger, f"Nothing to do, SECRET and SECRET_LIST parameters are empty!", logging.ERROR)
-        else:
+        if get_api_access_response.status_code != 200:
             utils.print_log(logger, f"Please check credentials, error {get_api_access_response.text}", logging.ERROR)
+            return
+            
+        if not SECRET_PATH and not MANAGED_ACCOUNT_PATH:
+            utils.print_log(logger, f"Nothing to do, SECRET and MANAGED_ACCOUNT parameters are empty!", logging.ERROR)
+            return
+
+        if SECRET_PATH:
+            secrets_safe_obj = secrets_safe.SecretsSafe(authentication=authentication_obj, logger=logger, separator=PATH_SEPARATOR)
+            get_secret_response = secrets_safe_obj.get_secret(SECRET_PATH)
+            append_output("secret", get_secret_response)
+        
+        if MANAGED_ACCOUNT_PATH:
+            managed_account_obj = managed_account.ManagedAccount(authentication=authentication_obj, logger=logger, separator=PATH_SEPARATOR)
+            get_managed_account_response = managed_account_obj.get_secret(MANAGED_ACCOUNT_PATH)
+            append_output("managed_account", get_managed_account_response)
+
     except Exception as e:
         utils.print_log(logger, e, logging.ERROR)
 
