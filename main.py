b732240c65c18fb7927657d5a65694a16428d5b7
Felipe Hernandez <fhernandez@beyondtrust.com>
2023-10-16 11:40:44 -0500

    change

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
 
         client_id = "6138d050-e266-4b05-9ced-35e7dd5093ae"
         add_mask(client_id)
-        set_output("cleint_id", client_id)
+        set_output("client_id", client_id)
 
     except Exception as e:
         utils.print_log(logger, e, logging.ERROR)
