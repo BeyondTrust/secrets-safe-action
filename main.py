1e25ab168d94bb92a565b9b451d7fc7950d9ca92
Felipe Hernandez <fhernandez@beyondtrust.com>
2023-10-17 15:00:34 -0500

    change

--- main.py
+++ main.py
@@ -63,21 +63,21 @@ def main():
             secrets_safe_obj = secrets_safe.SecretsSafe(authentication=authentication_obj, logger=logger, separator=PATH_SEPARATOR)
             get_secret_response = secrets_safe_obj.get_secret(SECRET_PATH)
             #_print_command("add-mask", get_secret_response, use_subprocess=False, escape_message=False)
-            set_output("secret", get_secret_response)
+            append_output("secret", get_secret_response)
         
         if MANAGED_ACCOUNT_PATH:
             managed_account_obj = managed_account.ManagedAccount(authentication=authentication_obj, logger=logger, separator=PATH_SEPARATOR)
             get_managed_account_response = managed_account_obj.get_secret(MANAGED_ACCOUNT_PATH)
             #_print_command("add-mask", get_managed_account_response, use_subprocess=False, escape_message=False)
-            set_output("managed_account", get_managed_account_response)
+            append_output("managed_account", get_managed_account_response)
 
 
         #_print_command(command="add-mask", command_message=CLIENT_ID, use_subprocess=False, escape_message=False)
-        set_output("client_id", CLIENT_ID)
+        append_output("client_id", CLIENT_ID)
 
         # masking certificate
         #_print_command(command="add-mask", command_message=CERTIFICATE, use_subprocess=True, escape_message=False)
-        set_output("certificate", CERTIFICATE)
+        append_output("certificate", CERTIFICATE)
 
 
     except Exception as e:
