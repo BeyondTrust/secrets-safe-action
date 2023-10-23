e1ba954fe29fdb74aaa8da610eb3a608b8b758aa
btfhernandez <133419363+btfhernandez@users.noreply.github.com>
2023-10-23 11:27:36 -0500

    feat: allow multiple secrets by step (#7)

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import sys
 import uuid
 import logging
+import json
 
-from secrets_safe_library import secrets_safe, authentication, utils, managed_account
+from secrets_safe_library import secrets, secrets_safe, authentication, utils, managed_account
 from github_action_utils import error
 
 env = os.environ
@@ -15,7 +16,6 @@ VERIFY_CA = True if 'VERIFY_CA' in env and env['VERIFY_CA'].lower() == 'true' el
 
 SECRET_PATH = env['INPUT_SECRET_PATH'].strip() if 'INPUT_SECRET_PATH' in env else None
 MANAGED_ACCOUNT_PATH = env['INPUT_MANAGED_ACCOUNT_PATH'].strip() if 'INPUT_MANAGED_ACCOUNT_PATH' in env else None
-
 PATH_SEPARATOR = env['PATH_SEPARATOR'].strip() if 'PATH_SEPARATOR' in env and len(env['PATH_SEPARATOR'].strip()) == 1 else "/"
 
 LOG_LEVEL = env['LOG_LEVEL'].strip().upper() if 'LOG_LEVEL' in env else "INFO"
@@ -49,6 +49,14 @@ CERTIFICATE_KEY = f"{CERTIFICATE_KEY}\n"
 COMMAND_MARKER: str = "::"
 
 def append_output(name, value):
+    """
+    Create a variable in step output.
+    Arguments:
+        output variable name
+        output content
+    Returns:
+    """
+
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
         delimiter = uuid.uuid1()
         print(f'{name}<<{delimiter}', file=fh)
@@ -56,8 +64,16 @@ def append_output(name, value):
         print(delimiter, file=fh)
 
 
-def print_command(command, command_message):
-    lines = command_message.split('\n')
+def mask_secret(command, secret_to_mask):
+    """
+    Mask secret to avoid print it out in console.
+    Arguments:
+        Masking command
+        Secret to mask
+    Returns:
+    """
+
+    lines = secret_to_mask.split('\n')
     for line in lines:
         if line.strip() != "":
             full_command = (
@@ -68,17 +84,50 @@ def print_command(command, command_message):
 
 
 def show_error(error_message):
+    """
+    Show error
+    Arguments:
+        Error message
+    Returns:
+    """
+    
     error(
-            error_message,
-            title="Action Failed",
-            col=1,
-            end_column=2,
-            line=4,
-            end_line=5,
+        error_message,
+        title="Action Failed",
+        col=1,
+        end_column=2,
+        line=4,
+        end_line=5,
         )
     utils.print_log(logger, error_message, logging.ERROR)
     sys.exit(1)
+    
+    
+def get_secrets(secret_obj, secrets):
+    """
+    Call secret safe library
+    Arguments:
+        Secret obj instance
+        List of secrets or managed acocunts
+    Returns:
+    """
 
+    try:
+        secrets_to_retrive = json.loads(secrets)
+    except Exception as e:
+        show_error(f"String could not be converted to JSON: {e}")
+    
+    if not isinstance(secrets_to_retrive, list):
+        secrets_to_retrive = [secrets_to_retrive]
+    
+    if len(secrets_to_retrive) > 20:
+        show_error("The Secrets Safe action can request a maximum of 20 secrets and 20 managed accounts each run")
+
+    for secret_to_retrieve in secrets_to_retrive:
+        get_secret_response = secret_obj.get_secret(secret_to_retrieve['path'])
+        mask_secret("add-mask", get_secret_response)
+        append_output(secret_to_retrieve['output_id'], get_secret_response)
+    
 
 def main():
     try:
@@ -96,23 +145,20 @@ def main():
         if get_api_access_response.status_code != 200:
             error_message = f"Please check credentials, error {get_api_access_response.text}"
             show_error(error_message)
-            
+        
         if not SECRET_PATH and not MANAGED_ACCOUNT_PATH:
             error_message = f"Nothing to do, SECRET and MANAGED_ACCOUNT parameters are empty"
             show_error(error_message)
 
         if SECRET_PATH:
             secrets_safe_obj = secrets_safe.SecretsSafe(authentication=authentication_obj, logger=logger, separator=PATH_SEPARATOR)
-            get_secret_response = secrets_safe_obj.get_secret(SECRET_PATH)
-            print_command("add-mask", get_secret_response)
-            append_output("secret", get_secret_response)
+            get_secrets(secrets_safe_obj, SECRET_PATH)
         
         if MANAGED_ACCOUNT_PATH:
             managed_account_obj = managed_account.ManagedAccount(authentication=authentication_obj, logger=logger, separator=PATH_SEPARATOR)
-            get_managed_account_response = managed_account_obj.get_secret(MANAGED_ACCOUNT_PATH)
-            print_command("add-mask", get_managed_account_response)
-            append_output("managed_account", get_managed_account_response)
-            
+            get_secrets(managed_account_obj, MANAGED_ACCOUNT_PATH)
+
+
     except Exception as e:
         show_error(e)
 
